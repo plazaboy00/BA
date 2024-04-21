02b25ba8d47a374036bb1e53f006ae2cd16e44a2
Linus Zielonka <lzielonka@ethz.ch>
2024-04-21 21:48:39 +0200

    Bus anpassung

--- src/main/python/bus_functions.py
+++ src/main/python/bus_functions.py
@@ -48,6 +48,81 @@ def compute_shortest_paths(G, points_gdf):
         route_lengths.append(route_length)
     return routes, route_lengths
 
+
+def load_geojson(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def passengers_on_bus(bus_stops_gdf, demand_geojson, destination_geojson):
+    passengers = []
+    bus_stop_coords = [(point.x, point.y) for point in bus_stops_gdf.geometry]
+
+    for demand_feature, dest_feature in zip(demand_geojson['features'], destination_geojson['features']):
+        passenger_origin = Point(demand_feature['geometry']['coordinates'])
+        passenger_destination = Point(dest_feature['geometry']['coordinates'])
+
+        origin_in_buffer = False
+        destination_in_buffer = False
+        for coord in bus_stop_coords:
+            bus_stop_point = Point(coord)
+            if bus_stop_point.buffer(500).contains(passenger_origin):
+                origin_in_buffer = True
+            if bus_stop_point.buffer(500).contains(passenger_destination):
+                destination_in_buffer = True
+            if origin_in_buffer and destination_in_buffer:
+                passengers.append({
+                    'origin': passenger_origin,
+                    'destination': passenger_destination,
+                    'timestamp': demand_feature['properties']['timestamp']
+                })
+                break
+
+    passengers_gdf = gpd.GeoDataFrame(passengers, geometry='origin')
+    return passengers_gdf
+
+def plot_passengers(passengers_gdf, bus_stops_gdf, demand_geojson, destination_geojson, gemeindegrenzen):
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    bus_stops_gdf.plot(ax=ax, color='grey', markersize=30, label='Bus Stops')
+
+    for feature in demand_geojson['features']:
+        point = Point(feature['geometry']['coordinates'])
+        ax.plot(point.x, point.y, 'go', markersize=6)
+
+    for feature in destination_geojson['features']:
+        point = Point(feature['geometry']['coordinates'])
+        ax.plot(point.x, point.y, 'bo', markersize=6)
+
+    origin_gdf = passengers_gdf[['origin', 'timestamp']].copy()
+    destination_gdf = gpd.GeoDataFrame()
+    destination_gdf['destination'] = None
+    destination_gdf['geometry'] = passengers_gdf['destination'].apply(Point)
+    destination_gdf = gpd.GeoDataFrame(destination_gdf)
+    destination_gdf.set_geometry('geometry', inplace=True)
+
+    origin_gdf.plot(ax=ax, color='orange', markersize=20, label='Origin Points', zorder=2)
+    destination_gdf.plot(ax=ax, color='yellow', markersize=20, label='Destination Points', zorder=2)
+
+    gemeindegrenzen.plot(ax=ax, color='black', alpha=0.5, label='Shapefile')
+
+    ax.set_title('Bus Stops, Demand and Destination Points, and Passengers')
+    ax.set_xlabel('Longitude')
+    ax.set_ylabel('Latitude')
+
+    demand_legend = plt.Line2D([], [], color='green', marker='o', markersize=8, linestyle='None', label='Demand Points')
+    destination_legend = plt.Line2D([], [], color='blue', marker='o', markersize=8, linestyle='None', label='Destination Points')
+    passengers_demand_legend = plt.Line2D([], [], color='orange', marker='o', markersize=8, linestyle='None', label='Passengers origin')
+    passengers_destination_legend = plt.Line2D([], [], color='yellow', marker='o', markersize=8, linestyle='None', label='Passengers destination')
+
+    ax.legend(handles=[demand_legend, destination_legend, passengers_demand_legend, passengers_destination_legend], loc='upper left')
+
+    plt.show()
+
+def count_passengers(passengers_gdf):
+    num_passengers = passengers_gdf.shape[0]
+    print("Anzahl der Passagiere im Bus:", num_passengers)
+
 # Funktion zum Plotten der Routen auf dem Straßennetzwerk
 def plot_routes(G, routes):
     fig, ax = ox.plot_graph_routes(G, routes, node_size=0)
--- src/main/python/busline.py
+++ src/main/python/busline.py
@@ -2,9 +2,14 @@ from bus_functions import *
 from plot import *
 
 ROOT_FILES = 'C:/Users/Linus/PycharmProjects/BA/'
+ROOT_DOCS = 'src/main/resources/Dokumente/'
 ROOT_Busstations = 'src/main/resources/Buslinie/Busstationen/'
 
-#Pfad zur Shapefile-Datei mit den Strassen
+# Lade und die Gemeindegrenzen
+shp_file = ROOT_FILES + 'src/main/resources/QGIS/Gemeindegrenzen/Grenzen_komp.shp'
+gemeindegrenzen = gpd.read_file(shp_file)
+
+# Pfad zur Shapefile-Datei mit den Strassen
 ROOT_RESOURCE_STRASSENNETZ = 'src/main/resources/QGIS/Strassen/'
 
 # Definieren des Bereichs für das Straßennetzwerk
@@ -23,10 +28,11 @@ bus_stops = gpd.read_file(ROOT_FILES + ROOT_Busstations + "Bushalte.shp")
 bus_stops_with_return = add_return_trip(bus_stops)
 
 # Konvertierung der Bushaltestellenprojektion von LV95 nach WGS84
-bus_stops_with_return['geometry'] = bus_stops_with_return['geometry'].apply(lv95_to_wgs84)
+bus_stops_with_return_wgs84 = bus_stops_with_return.copy()
+bus_stops_with_return_wgs84['geometry'] = bus_stops_with_return_wgs84['geometry'].apply(lv95_to_wgs84)
 
 # Berechnung der kürzesten Routen zwischen den Haltestellen
-routes, route_lengths = compute_shortest_paths(G, bus_stops_with_return)
+routes, route_lengths = compute_shortest_paths(G, bus_stops_with_return_wgs84)
 
 # Plotten der Routen auf dem Straßennetzwerk
 plot_routes(G, routes)
@@ -34,3 +40,15 @@ plot_routes(G, routes)
 # Berechnung der Gesamtlänge der Route
 total_route_length = sum(route_lengths) / 1000
 print("Gesamtlänge der Route:", total_route_length, "Kilometer")
+
+
+file_path_demand = ROOT_FILES + ROOT_DOCS + "Nachfrage.geojson"
+file_path_destination = ROOT_FILES + ROOT_DOCS + "Ziele.geojson"
+demand_geojson = load_geojson(file_path_demand)
+destination_geojson = load_geojson(file_path_destination)
+
+passengers_gdf = passengers_on_bus(bus_stops_with_return, demand_geojson, destination_geojson)
+
+plot_passengers(passengers_gdf, bus_stops_with_return, demand_geojson, destination_geojson, gemeindegrenzen)
+
+count_passengers(passengers_gdf)
